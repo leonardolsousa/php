ad467f9048d75318d193db8c6d1ea530b0a4fe3c
Leonardo <leonardolsousa@hotmail.com>
2018-11-04 20:35:10 -0200

    Utilização de variáveis para salvar dados pessoais

---
+++ Variaveis.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = "João Pedro";
+$sobrenome = "Santos Silva";
+$endereco = "Rua das Couves";
+$idade = 27;
+$telefone = 999999999;
+
+echo "Meu nome é ".$nome." e sobrenome ".$sobrenome.".<br>";
+echo "Moro na ".$endereco.".<br>";
+echo "Tenho ".$idade." anos. <br>";
+echo "Meu telefone é ".$telefone.".<br>";
+
+?>

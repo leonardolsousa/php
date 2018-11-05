6811609d0e134ff741a711f456e3ec39da42d7cb
Leonardo <leonardolsousa@hotmail.com>
2018-11-05 21:37:22 -0200

    Uso de Switch em cardápio

---
+++ Switch.php
@@ -0,0 +1,27 @@
+<?php
+
+$opcao = 5;
+
+switch($opcao){
+    case(1):
+        echo "Strogonoff de frango";
+        break;
+    case(2):
+        echo "Bife acebolado";
+        break;
+    case(3):
+        echo "Salada";
+        break;
+    case(4):
+        echo "Filé com fritas";
+        break;
+    case(5):
+        echo "Feijoada";
+        break;
+    case(6):
+        echo "Risoto de 4 queijos";
+        break;
+
+}
+
+?>

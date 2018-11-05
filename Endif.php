e7a9c5d333dd036cee85cd3d23a88ec902e1ddbe
Leonardo <leonardolsousa@hotmail.com>
2018-11-05 19:23:16 -0200

    Utilização do Elseif com dois pontos e endif

---
+++ Endif.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = "João Lucas";
+$salario = 1999;
+
+if($salario < 1000):
+    echo "Uau! Você não vai pagar nada de imposto!";
+elseif($salario < 1500):
+    echo "Eita! Você paga 10% de imposto!";
+elseif($salario < 2000):
+    echo "Caraca! Você paga 15% de imposto!";
+else:
+    echo "Eita pega! Você paga 20% de imposto!";
+endif;
+?>

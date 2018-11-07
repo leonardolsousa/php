cff9da222b86635ce180298f26c879cf5c2a4c00
Leonardo <leonardolsousa@hotmail.com>
2018-11-07 20:47:37 -0200

    Listagem de arrays com foreach de times da Serie A do Brasileiro

---
+++ Foreach.php
@@ -0,0 +1,30 @@
+<?php
+
+$times = array(
+    "Santos",
+    "São Paulo",
+    "Cruzeiro",
+    "Flamengo",
+    "Fluminense",
+    "Botafogo",
+    "Vasco",
+    "Atlético-MG",
+    "Atlético-PR",
+    "Corinthians",
+    "Grêmio",
+    "Internacional",
+    "Bahia",
+    "Vitória",
+    "Ceará",
+    "Sport",
+    "América-MG",
+    "Chapecoense",
+    "Paraná",
+    "Palmeiras"
+);
+
+foreach($times as $time){
+    echo "Times do Brasileirão da Série A: ".$time."<br>";
+}
+
+?>

c87a1ab4290295ceba39e25facaa34059eceb599
Leonardo <leonardolsousa@hotmail.com>
2018-11-06 23:41:16 -0200

    Exercicio 4

---
+++ Exercicio4.php
@@ -0,0 +1,46 @@
+<?php
+
+$mes = 7;
+
+switch($mes){
+    case(1):
+        echo "Mês: Janeiro";
+        break;
+    case(2):
+        echo "Mês: Fevereiro";
+        break;
+    case(3):
+        echo "Mês: Março";
+        break;
+    case(4):
+        echo "Mês: Abril";
+        break;
+    case(5):
+        echo "Mês: Maio";
+        break;
+    case(6):
+        echo "Mês: Junho";
+        break;
+    case(7):
+        echo "Mês: Julho";
+        break;
+    case(8):
+        echo "Mês: Agosto";
+        break;
+    case(9):
+        echo "Mês: Setembro";
+        break;
+    case(10):
+        echo "Mês: Outubro";
+        break;
+    case(11):
+        echo "Mês: Novembro";
+        break;
+    case(12):
+        echo "Mês: Dezembro";
+        break;
+    default:
+        echo "Não existe mês com esse número! Tem que ser entre 1 e 12!";
+}
+
+?>

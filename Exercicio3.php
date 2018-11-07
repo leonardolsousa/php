bbf4d334208e6296564837841be6ba670e9e8b91
Leonardo <leonardolsousa@hotmail.com>
2018-11-06 23:27:16 -0200

    Exercicio 3

---
+++ Exercicio3.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = "Juliana";
+$sexo = "Feminino"; 
+$idade = 24;
+
+if($sexo == "Feminino" && $idade < 25){
+    echo $nome." : ACEITA!";
+}else {
+    echo $nome." : NÃO ACEITA!";
+}
+
+?>

09fc70d09c8c13d4d2d97bf93aded46b356933e1
Leonardo <leonardolsousa@hotmail.com>
2018-11-07 03:57:10 -0200

    Lista de 10 Funcionários

---
+++ Lista.php
@@ -0,0 +1,8 @@
+<?php
+
+$funcionarios = array("João", "Pedro", "Lucas", "Gabriel", "Leonardo", "Rodrigo", "Mateus", "Marcos", "José", "Francisco");
+
+echo "Os 10 funcionários da fábrica de papel são: ".$funcionarios[0]." , ".$funcionarios[1]." , ".$funcionarios[2]." , ".$funcionarios[3]." , ";
+echo $funcionarios[4]." , ".$funcionarios[5]." , ".$funcionarios[6]." , ".$funcionarios[7]." , ";
+echo $funcionarios[8]." , ".$funcionarios[9].".";
+?>

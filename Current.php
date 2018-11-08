e5e9ce8d26d934a9ca1b1283ad9e271ef6dbdc9e
Leonardo <leonardolsousa@hotmail.com>
2018-11-07 23:27:55 -0200

    Lista de array com current e next

---
+++ Current.php
@@ -0,0 +1,27 @@
+<?php
+
+$capitais = array(
+    "São Paulo",
+    "Rio de Janeiro",
+    "Belo Horizonte",
+    "Brasília",
+    "Salvador",
+    "Fortaleza",
+    "Palmas",
+    "Manaus",
+    "Porto Alegre",
+    "Belém"
+);
+
+echo current($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+
+?>

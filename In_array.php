8da6a4a57ccb20a3708923a86490c2e210d72229
Leonardo <leonardolsousa@hotmail.com>
2018-11-08 00:21:15 -0200

    Busca de valor dentro da array

---
+++ In_array.php
@@ -0,0 +1,39 @@
+<?php
+
+$capitais = array(
+    "Rio Branco",
+    "Maceió",
+    "Macapá",
+    "Manaus",
+    "Salvador",
+    "Fortaleza",
+    "Brasília",
+    "Vitória",
+    "Goiânia",
+    "São Luís",
+    "Cuiabá",
+    "Campo Grande",
+    "Belo Horizonte",
+    "Belém",
+    "João Pessoa",
+    "Curitiba",
+    "Recife",
+    "Teresina",
+    "Rio de Janeiro",
+    "Natal",
+    "Porto Alegre",
+    "Porto Velho",
+    "Boa Vista",
+    "Florianopólis",
+    "São Paulo",
+    "Aracaju",
+    "Palmas"
+);
+
+if(in_array("Guarujá",$capitais)){
+    echo "Esta cidade é uma capital do Brasil!";
+}else{
+    echo "Esta cidade não é uma capital do Brasil!";
+}
+
+?>

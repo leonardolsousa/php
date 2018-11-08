dddfa39ec15d402a9b7cb83322e75d8f1bcba66f
Leonardo <leonardolsousa@hotmail.com>
2018-11-08 19:26:40 -0200

    Escondendo as informações do Form via POST

---
+++ Formalunos2.php
@@ -0,0 +1,9 @@
+<form action="processa_form2.php" method="POST">
+    <label>Nome</label><br>
+    <input type="text" name="nome" /><br>
+    <label>Turma</label><br>
+    <input type="text" name="turma" /><br>
+    <label>Senha</label><br>
+    <input type="password" name="senha" /><br>
+    <input type="submit" value="Enviar dados" /><br>
+</form>
---
+++ processa_form2.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Formulário de Alunos <br>";
+echo "Nome: ".$_POST["nome"]."<br>";
+echo "Turma: ".$_POST["turma"]."<br>";
+echo "Senha: ".$_POST["senha"]."<br>";
+
+?>

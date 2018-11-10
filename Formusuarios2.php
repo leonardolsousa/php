93cb05664b8d89eea9cee9e900bc5abfa88aac8a
Leonardo <leonardolsousa@hotmail.com>
2018-11-10 11:55:39 -0200

    Inserindo novos usuarios no banco de dados através do formulário

---
+++ Formusuarios2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cadastro de Usuários</title>
+    </head>
+    <body>
+        <form action="gravar_usuario2.php" method="POST">
+            <label>Email</label><br>
+            <input type="text" name="email"/><br><br>
+
+            <label>Senha</label><br>
+            <input type="password" name="pass"/><br><br>
+
+            <label>Status</label><br>
+            <select name="status">
+                <option value="0">0 (Inativo)</option>
+                <option value="1">1 (Ativo)</option>
+            </select>
+            <br><br>
+
+            <input type="submit" value="Gravar dados"/><br>
+
+        </form>
+    </body>
+</html>
